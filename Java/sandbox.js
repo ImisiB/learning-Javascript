19f9b495ac7c6ae359046505f8e5a11a2b6ef7c0
Imisi Bailey <imisibailey@gmail.com>
2025-06-24 22:05:03 +0100

    Learning javascript

--- Java/sandbox.js
+++ Java/sandbox.js
@@ -102,9 +102,39 @@ const likes = 30;
 // console.log(result);
 
 // //Creating html templates
-let html = `
-  <h2>${title}</h2>
-  <p> By ${author}</p>
-  <span>This blog has ${likes} likes</span>
-`;
-console.log(html);
+// let html = `
+//   <h2>${title}</h2>
+//   <p> By ${author}</p>
+//   <span>This blog has ${likes} likes</span>
+// `;
+// console.log(html);
+
+
+
+
+// Arrays
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken'
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length)
+
+// //
+
+// let result = ninjas.join('-');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal'])
+let result = ninjas.push('ken');
+
+console.log(ninjas);
+
+
+
+
